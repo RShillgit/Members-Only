16044235f5b63b3cf89150d592bcb3b4436495d7
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-02-21 08:45:25 -0500

    Schema Update

--- models/messages.js
+++ models/messages.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: "User" },
     title: { type: String, required: true, maxLength: 50 },
     text: { type: String, required: true },
     timestamp: { type: Date, required: true }
